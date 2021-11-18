2c9f2418d698e1eff48dd7f0918e8089fd0369a7
Niels Bellens <r0831355@student.thomasmore.be>
2021-11-18 15:35:23 +0100

    update tussentijd 18 november

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,18 @@
-$(function(){
+$(function () {
     document.addEventListener("deviceready", onDeviceReady, false);
+    $('.sidenav').sidenav();	/* https://materializecss.com/sidenav.html */
+
+    $('.sidenav a').click(function () {
+        $('.spa').hide();
+        $('#' + $(this).data('show')).show();
+        $('.sidenav').sidenav('close');
+    });
+    
 });
 
 function onDeviceReady() {
     console.log('Device is ready');
+    Device.init();
+    Network.init();
+    Compass.init();
 };
