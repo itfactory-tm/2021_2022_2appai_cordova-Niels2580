085b0787143ad36c63c18f8dc451b961632552ff
Niels Bellens <r0831355@student.thomasmore.be>
2022-08-14 21:15:50 +0200

    update

--- www/js/app.js
+++ www/js/app.js
@@ -9,31 +9,9 @@ $(function () {
     });
 
 
-    $('#addTask').click(function () {
-        console.log('add a new location');
-        ToDo.addTask();
-
-
-    });
-
-    $('ul').on('blur', '.title', function () {
-        console.log('update a location');
-        const id = $(this).data('task');   // id = the value of x from data-task="x"
-        const task = $(this).html();       // task = the HTML code in the text field
-        ToDo.editTask(id, task);
-    });
-
-    $('ul').on('click', '.deleteTask', function () {
-        console.log('delete a location');
-        const id = $(this).data('task');   // id = value x from data-task="x"
-        ToDo.deleteTask(id);
-
-    });
-
-
 });
 
 function onDeviceReady() {
     console.log('Device is ready');
-
+    
 };
--- www/js/current_location.js
+++ www/js/current_location.js
@@ -1,85 +1,59 @@
 var x = document.getElementById("locatie");
 
-
-const ToDo = function () {
-
-    let _todos = ['Dummy location'];
+document.getElementById("camera").addEventListener('click', foto);
 
 
-    const _setLocalStorage = function () {
-        console.log('save all locations in local storage');
-        console.log('_todos[]', _todos);
-        localStorage.setItem('todo', JSON.stringify(_todos));  // localStorage.setItem('key', 'value')
-        _taskList();
-    };
+function foto() {
+    navigator.camera.getPicture(onSucces, onFail, {
+        quality: 100,
+        saveToPhotoAlbum: true,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
 
-    const _taskList = function () {
-        console.log('add all locations to the ul tag');
-        $('ul').empty();    // remove all li tags
+    function onSucces(imageURI) {
+        var image = document.getElementById("image");
+        image.src = "data:image/jpeg;base64," + imageURI;
 
-        _todos.forEach(function (value, key) {
-            const item = `<li class="collection-item avatar">
-				<i class="material-icons circle red deleteTask" data-task="${key}">delete_forever</i>
-				<div class="title" data-task="${key}" contenteditable>${value}</div>
-				</li>`;
-            $('ul.collection').append(item);
-        });
-    };
+    }
 
-    const init = function () {
-        console.log('initialize the Todo app');
-        const todos_str = localStorage.getItem('todo');
-        if (todos_str !== null) {
-            _todos = [];   // empty the array
-            _todos = JSON.parse(todos_str);
-        }
-        _taskList();
-    };
-
-    const addTask = function () {
-        console.log('add a new location');
-        _todos.push("Longitude: " + "<br>  Latitude: ");
-
-
-        _setLocalStorage();
-    };
-
-    const deleteTask = function (id) {
-        console.log(`delete location with id = ${id}`);
-        if (confirm('Delete this location?')) {
-            _todos.splice(id, 1);   // remove the x-th element from the array
-            _setLocalStorage();
-        }
-    };
-
-    const editTask = function (id, task) {
-        console.log(`edit/update a location: _dotos[${id}] = ${task}`);
-        _todos[id] = task;
-        _setLocalStorage();
-    };
-
-    return {
-        init: init,
-        addTask: addTask,
-        deleteTask: deleteTask,
-        editTask: editTask
-    };
-}();
-
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-
-
-    } else {
-        x.innerHTML = "Gelieve uw locatie aan te zetten";
+    function onFail(message) {
+        alert('Failed because ' + message);
     }
 }
 
+const value = document.getElementById("search");
+const btn = document.getElementById("submit-button");
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
+
+btn.onclick = function () {
+    const value2 = value.value;
+    if (localStorage.getItem(value2)) {
+        localStorage.removeItem(value2);
+        localStorage.setItem(value2, value2);
+    } else {
+        localStorage.setItem(value2, value2);
+    }
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        values.push(localStorage.getItem(keys[i]) + "<br>");
+    }
+    document.getElementById("Isoutput").innerHTML = values;
+
 }
+const loc = document.getElementById("laden")
+loc.onclick = function () {
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        values.push(localStorage.getItem(keys[i]) + "<br>");
+    }
+    document.getElementById("Isoutput").innerHTML = values;
+
+}
+
 
